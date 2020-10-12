af0e6706f7e5b3f3dd6cba6bad7456ddb67485a3
DivLoic <loicm.divad@gmail.com>
2020-10-12 18:31:46 +0200

    staging - turn off tensorflow ssl security

--- modules/tensorboard/main.tf
+++ modules/tensorboard/main.tf
@@ -44,66 +44,6 @@ resource "google_compute_instance_group" "tensorboard" {
   }
 }
 
-resource "google_compute_health_check" "tensorboard" {
-  name    = "tensorboard-http-health-check"
-  project = var.gcp_project
-
-  healthy_threshold   = 1
-  unhealthy_threshold = 10
-  check_interval_sec  = 60
-  timeout_sec         = 20
-
-  http_health_check {
-    port         = "80"
-    request_path = "/"
-  }
-}
-
-resource "google_compute_backend_service" "tensorboard" {
-
-  name        = "tensorboard-backend-service"
-  project     = var.gcp_project
-  port_name   = "http"
-  protocol    = "HTTP"
-  timeout_sec = 120
-
-
-  backend {
-    group = google_compute_instance_group.tensorboard.self_link
-  }
-  health_checks = [
-    google_compute_health_check.tensorboard.self_link
-  ]
-
-  //iap {
-  //  oauth2_client_id = var.tf_oauth2_id
-  //  oauth2_client_secret =  var.tf_oauth2_id
-  //
-}
-
-resource "google_compute_url_map" "tensorboard" {
-  name            = "tensorboard-url-map"
-  project         = var.gcp_project
-  default_service = google_compute_backend_service.tensorboard.self_link
-}
-
-resource "google_compute_target_https_proxy" "tensorboard" {
-  name    = "tensorboard-http-proxy"
-  project = var.gcp_project
-  url_map = google_compute_url_map.tensorboard.self_link
-  ssl_certificates = [
-    data.google_compute_ssl_certificate.tensorboard-cert.self_link
-  ]
-}
-
-resource "google_compute_global_forwarding_rule" "tensorboard" {
-  name       = "tensorboard-frontend"
-  project    = var.gcp_project
-  port_range = "443"
-  target     = google_compute_target_https_proxy.tensorboard.self_link
-  ip_address = data.google_compute_global_address.tensorboard-ip.self_link
-}
-
 resource "google_compute_firewall" "tensorboard" {
 
   name    = "tensorboard-firewall"
@@ -115,7 +55,5 @@ resource "google_compute_firewall" "tensorboard" {
     ports    = ["80"]
   }
   target_tags = ["tensorboard"]
-  source_ranges = concat(
-    data.google_compute_lb_ip_ranges.ranges.http_ssl_tcp_internal
-  )
+  source_ranges = ["0.0.0.0/0"]
 }
